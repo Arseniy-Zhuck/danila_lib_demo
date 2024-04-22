70e9fbae6c8db2c81a63cc24d20dce1efc337f70
Arseniy-Zhuck <arseniyzhuck@mail.ru>
2024-04-22 15:50:27 +0300

    rama-no-spring-detect-1.2.0

--- demo_1/step_1.py
+++ demo_1/step_1.py
@@ -3,7 +3,7 @@ from danila.danila import Danila
 
 img_path = 'demo_1/example.jpeg'
 img = cv2.imread(img_path)
-danila = Danila()
+danila = Danila('yolov5')
 res = danila.rama_classify(img)
 cv2.namedWindow(res, cv2.WINDOW_AUTOSIZE)
 cv2.imshow(res, img)
