36d0318707ea1672c8e2bda7b54ebd40c554dda9
Arseniy-Zhuck <arseniyzhuck@mail.ru>
2024-05-13 09:29:48 +0300

    text-recognize-1.4.1

--- demo/step_6.py
+++ demo/step_6.py
@@ -2,9 +2,9 @@ import cv2
 
 from danila.danila import Danila
 
-img_path = 'demo/example.jpeg'
+img_path = 'demo/example_1.jpeg'
 img = cv2.imread(img_path)
-danila = Danila('yolov5')
+danila = Danila(2,'yolov5')
 labels = danila.text_recognize(img)
 print(labels)
 
